20dd9799af75e5f9c156f1f8ffd0c47acb49e8b9
ISHA675 <kcisha675@gmail.com>
2024-12-09 15:37:20 -0600

    Loop file

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+
+public class Program{
+    public static void Main (string [ ] args){
+        Console.WriteLine("Enter a number: ");
+        var a=Convert.ToInt32(Console.ReadLine());
+        for (int i=1;i<=10;i++){
+            Console.WriteLine(a +"*"+i+"="+a*i);
+        }
+    }
+}
