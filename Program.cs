cf325495d8aa3c87a964313ff0e80ca8b82e240e
ISHA675 <kcisha675@gmail.com>
2024-12-10 14:48:13 -0600

    Created multiplication table program

--- Program.cs
+++ Program.cs
@@ -6,8 +6,14 @@ public class Program{
     public static void Main (string [ ] args){
         Console.WriteLine("Enter a number: ");
         var a=Convert.ToInt32(Console.ReadLine());
+        table_multiply(a);
+        
+    }
+    static void table_multiply(int a){
+        Console.WriteLine($"Multiplication Table for {a}\n");
         for (int i=1;i<=10;i++){
-            Console.WriteLine(a +"*"+i+"="+a*i);
+            Console.WriteLine(a*i);
         }
+        
     }
 }
--- obj/Debug/net8.0/Loops.AssemblyInfo.cs
+++ obj/Debug/net8.0/Loops.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Loops")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a1e394dde44bd0938879d223adeed5579b84468")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec97c53d008b89f65a959f2d3752b02c2018ca34")]
 [assembly: System.Reflection.AssemblyProductAttribute("Loops")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Loops")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
